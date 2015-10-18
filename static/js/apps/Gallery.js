177d8554ff9edceb4cc0504e0d2fce884f5bd3a7
nex2hex <nex2hex@ya.ru>
2015-10-18 21:51:41 +0500

    js 2

--- static/js/apps/Gallery.js
+++ static/js/apps/Gallery.js
@@ -1,6 +1,14 @@
 import ReactDOM from 'react-dom';
 
+import GalleryApiActions from '../actions/GalleryApiActions';
+
 export default function (options) {
+	if (!options.photos) {
+		return;
+	}
+
+	GalleryApiActions.reciveInitPhotos(options.photos);
+
 	ReactDOM.render(
 		<Gallery />,
 		document.getElementsByClassName('i-timeline')[0]
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
 import '../css/index.styl';
 
 // import React from 'react'; window.React = React;
-import UploadPhoto from './apps/UploadPhoto';
-import Gallery from './apps/Gallery';
+import UploadPhotoApp from './apps/UploadPhoto';
+import GalleryApp from './apps/Gallery';
 
 window.apps = {
-	UploadPhoto: UploadPhoto,
-	Gallery: Gallery
+	UploadPhoto: UploadPhotoApp,
+	Gallery: GalleryApp
 };
