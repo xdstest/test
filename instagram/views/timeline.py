a370ffb8960f862521d1e30c63521d7f60984d64
nex2hex <nex2hex@ya.ru>
2015-10-18 19:54:37 +0500

    gallery_data 2

--- instagram/views/timeline.py
+++ instagram/views/timeline.py
@@ -29,23 +29,28 @@ class BaseTimeline(TemplateView):
     def _photos_set(self, **kwargs):
         return Photo.objects
 
-    def gallery_json_data(self, context):
+    def get_context_photos_data(self, context, **kwargs):
+        photos = self._photos_set(**kwargs)
+        if not context['user_can_edit_photos']:
+            photos = photos.filter(visibility=Photo.VISIBILITY_PUBLIC)
+        return photos.order_by('-created')[:self.ITEMS_PER_PAGE]
+
+    def get_context_gallery_json_data(self, context, **kwargs):
         gallery_data = {
             'userCanEditPhotos': context['user_can_edit_photos'],
             'apiEndpoint': '/api' + self.request.path,
             'photos': [photo.as_dict() for photo in context['photos']]
         }
-        context['gallery_data'] = json.dumps(gallery_data, ensure_ascii=False)
+        return json.dumps(gallery_data, ensure_ascii=False)
 
+    def get_context_user_can_edit_photos(self, context, **kwargs):
+        return self.request.user.is_authenticated() and self.request.user.is_moderator
 
     def get_context_data(self, **kwargs):
         context = {}
-        context['user_can_edit_photos'] = self.request.user.is_authenticated() and self.request.user.is_moderator
-        photos = self._photos_set(**kwargs)
-        if not context['user_can_edit_photos']:
-            photos = photos.filter(visibility=Photo.VISIBILITY_PUBLIC)
-        context['photos'] = photos.order_by('-created')[:self.ITEMS_PER_PAGE]
-        self.gallery_json_data(context)
+        context['user_can_edit_photos'] = self.get_context_user_can_edit_photos(context, **kwargs)
+        context['photos'] = self.get_context_photos_data(context, **kwargs)
+        context['gallery_data'] = self.get_context_gallery_json_data(context, **kwargs)
         return context
 
 
@@ -56,18 +61,26 @@ class TagTimeline(TagTimelineMixin, BaseTimeline):
 class UserTimeline(UploadPhoto, BaseTimeline):
     template_name = 'timeline/user_timeline.jinja2'
 
-    def get_context_data(self, **kwargs):
-        context = {}
-        context['timeline_user'] = get_object_or_404(User, username=kwargs.get('username'))
-        context['user_can_edit_photos'] = self.request.user.is_authenticated() and \
-                            (self.request.user.is_moderator or self.request.user.id == context['timeline_user'].id)
+    def get_context_photos_data(self, context, **kwargs):
         photos = self._photos_set(**kwargs).filter(user=context['timeline_user'])
         if not context['user_can_edit_photos']:
             photos = photos.exclude(visibility=Photo.VISIBILITY_PRIVATE)
+        return photos.order_by('-created')[:self.ITEMS_PER_PAGE]
+
+    def get_context_user_can_edit_photos(self, context, **kwargs):
+        return self.request.user.is_authenticated() and (
+            self.request.user.is_moderator or self.request.user.id == context['timeline_user'].id)
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['timeline_user'] = get_object_or_404(User, username=kwargs.get('username'))
         context['timeline_user_posts'] = Photo.objects.filter(user=context['timeline_user']).count()
-        context['photos'] = photos.order_by('-created')[:self.ITEMS_PER_PAGE]
         context['form'] = self.get_form()
-        self.gallery_json_data(context)
+
+        context['user_can_edit_photos'] = self.get_context_user_can_edit_photos(context, **kwargs)
+        context['photos'] = self.get_context_photos_data(context, **kwargs)
+        context['gallery_data'] = self.get_context_gallery_json_data(context, **kwargs)
+
         return context
 
 
